8a8fec26056a2b201c80636300d8795a97b6f144
Abbas Mammadov <abbasmammadov@Abbass-MacBook-Pro.local>
2022-08-21 08:28:09 +0900

    add ?register and ?reset

--- attendance/main.js
+++ attendance/main.js
@@ -14,7 +14,55 @@ App.onJoinPlayer.Add(function(p) {
 
 App.onSay.Add(function(player, text) {
     let command = text.split(" ");
-    if (command[0] == "?show") {
+
+	if ((command[0] == "?register") && (player.role > 0)) {
+		let players_reg = App.players;
+		let check_reg = JSON.parse(App.storage);
+		let check_reg_str = App.storage;
+		let checking = true;
+		let temp_array = {"players": []};
+
+		for (let i in players_reg) {
+			player_check = players_reg[i];
+			current_player = player_check.name;
+			// let check_reg = JSON.parse(App.storage);
+			// let check_reg_str = App.storage;
+			// let checking = true;
+			checking = true;
+			for([key, val] of Object.entries(check_reg)) {
+				if (current_player == key) {
+					checking = false;
+				}
+			}
+
+			if (checking) {
+				temp_array.players.push(current_player);
+			}
+			// if (checking) {
+			// 	let latest = check_reg_str.substring(1, check_reg.length-1);
+			// 	let registered = `{${latest}"${current_player}": {"attended": "${0}", "late": "${0}", "absent": "${0}"},\n}`;
+			// 	App.storage = registered;
+			// 	App.save();
+			// }
+		}
+
+		let registered = '';
+		// addin players in temp_array to App.storage
+		for (let i in temp_array.players) {
+			registered += `"${temp_array.players[i]}": {"attended": "${0}", "late": "${0}", "absent": "${0}"},\n`;
+		}
+		let latest = check_reg_str.substring(1, check_reg_str.length-1);
+		// App.sayToAll("###########");
+		// App.sayToAll(latest);
+		// App.sayToAll("###########");
+		let new_storage = `{${registered}${latest}}`;
+		// App.sayToAll(new_storage);
+		App.storage = new_storage;
+		App.save();
+
+
+	}
+    else if (command[0] == "?show") {
         App.sayToAll("Command 'show' received");
         App.sayToAll(App.storage);
         let stored = JSON.parse(App.storage);
@@ -91,14 +139,31 @@ App.onSay.Add(function(player, text) {
 		App.showCenterLabel("You have already submitted your attendance");
 	}
 	else if (command[0] == "?stop" && (player.role > 0)){
-		let updated_storage = JSON.parse(App.storage);
-		for (let player of App.players) {
-			if ((player.tag.checked == false)) {
-				let foo = parseInt(updated_storage[curr_player].absent);
-				updated_storage[curr_player].absent = foo + 1;
+		let stop_storage = JSON.parse(App.storage);
+		//for([key, val] of Object.entries(stop_storage)) {
+		//	if (player.name == key) {
+		//		checker = false;
+		//	}
+		//}
+		let players = App.players;
+		for (let i in players) {
+			player_check = players[i];
+			curr_player = player_check.name;
+			if ((player_check.tag.checked == false)) {
+				let foo = parseInt(stop_storage[curr_player].absent);
+				stop_storage[curr_player].absent = foo + 1;
 			}	
 		}
-		App.storage = JSON.stringify(updated_storage);
+		App.storage = JSON.stringify(stop_storage);
 		App.save();
+	}
+
+	else if (command[0] == "?reset" && (player.role > 0)){
+		App.storage = "{}";
+		App.save();
+	}
+	
+	else if (command[0] == "?test") {
+		App.showCenterLabel("Working")
 	}
 })
